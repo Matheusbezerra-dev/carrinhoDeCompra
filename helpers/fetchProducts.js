f43e7e833a656633637458783cd16f2c2e780594
Matheus Bezerra <mabezerra2@gmail.com>
2022-07-11 17:31:22 -0400

    Caraca Trybe...,exercicio 1 finalizado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+const fetchProducts = async (productName) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${productName}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};  
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,20 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se "fetchProducts" é uma função', () =>
+    expect(typeof fetchProducts).toEqual('function')
+  );
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it('ao chamar a função "fetchProducts" com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computado"', () =>{
+    fetchProducts('computador');
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(url)
+  });
+  it('o retorno da função "fetchProducts" com o argumento "computador" é uma estrutura de dados igual ao objeto "computadorSearch", que já está importado no arquivo.', async () => 
+    expect(await fetchProducts('computador')).toEqual(computadorSearch));
+  it('ao chamar a função "fetchProducts" sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => 
+    expect(await fetchProducts()).toEqual(new Error("You must provide an url")));
 });
